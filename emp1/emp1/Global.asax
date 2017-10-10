9d39f72177d6d65dbc4b1021d9df6b3f399f9587
rizalnoor <rizalnoorrohmat@outlook.com>
2017-10-10 13:56:43 +0700

    meaningful commit message

---
+++ emp1/emp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="emp1.MvcApplication" Language="C#" %>
---
+++ emp1/emp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(emp1.Startup))]
+namespace emp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
